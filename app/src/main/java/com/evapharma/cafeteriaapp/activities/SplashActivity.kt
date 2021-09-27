cf7435ab383a7d37a94de6cf144982ebe8c527bc
Kerollos Ragaie <kerolosragaie@gmail.com>
2021-09-27 20:44:28 +0200

    Finishing setups

--- app/src/main/java/com/evapharma/cafeteriaapp/activities/SplashActivity.kt
+++ app/src/main/java/com/evapharma/cafeteriaapp/activities/SplashActivity.kt
@@ -1,11 +1,13 @@
-package com.evapharma.cafeteriaapp
+package com.evapharma.cafeteriaapp.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.evapharma.cafeteriaapp.R
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
     }
+
 }
