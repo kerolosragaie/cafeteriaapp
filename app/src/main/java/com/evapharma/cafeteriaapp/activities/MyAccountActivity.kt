4785ad01669926b308e1f3f2e78338f327fa2d40
MohammedAbusarie <new.is.new55@gmail.com>
2021-10-08 00:53:21 +0200

    Added MyProfile Activity + Runda font added but not recognized

--- app/src/main/java/com/evapharma/cafeteriaapp/activities/MyAccountActivity.kt
+++ app/src/main/java/com/evapharma/cafeteriaapp/activities/MyAccountActivity.kt
@@ -2,14 +2,14 @@ package com.evapharma.cafeteriaapp.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.evapharma.cafeteriaapp.databinding.ActivityHomeBinding
+import com.evapharma.cafeteriaapp.databinding.ActivityMyAccountBinding
 
 class MyAccountActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityHomeBinding
+    private lateinit var binding: ActivityMyAccountBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityHomeBinding.inflate(layoutInflater)
+        binding = ActivityMyAccountBinding.inflate(layoutInflater)
         setContentView(binding.root)
     }
 }
