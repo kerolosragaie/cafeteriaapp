e6b7f9e2884ff0e0bdc79bc0bba2112d6b0725e4
MohammedAbusarie <new.is.new55@gmail.com>
2021-10-09 19:32:23 +0200

    OrderDetails Binding :)

--- app/src/main/java/com/evapharma/cafeteriaapp/activities/OrderDetailsActivity.kt
+++ app/src/main/java/com/evapharma/cafeteriaapp/activities/OrderDetailsActivity.kt
@@ -9,6 +9,7 @@ import android.widget.TextView
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.evapharma.cafeteriaapp.R
+import com.evapharma.cafeteriaapp.databinding.ActivityOrderDetailsBinding
 import com.evapharma.cafeteriaapp.helpers.OrderDetailsItemsAdapter
 import com.evapharma.cafeteriaapp.models.OrderDetailsItem
 import com.google.android.material.appbar.AppBarLayout
@@ -18,9 +19,17 @@ import com.evapharma.cafeteriaapp.orderDetailsList
 
 
 class OrderDetailsActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityOrderDetailsBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_order_details)
+
+        binding= ActivityOrderDetailsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+
 
             initCollapsingToolbar()
             initButtons()
@@ -30,20 +39,20 @@ class OrderDetailsActivity : AppCompatActivity() {
         }
 
     private fun initCollapsingToolbar(){
-        findViewById<CollapsingToolbarLayout>(R.id.ct_orderdetails).apply{
+        binding.ctOrderdetails.apply{
             setExpandedTitleTextAppearance(R.style.collapsingToolbarLayoutTitleColor);
             setCollapsedTitleTextAppearance(R.style.collapsingToolbarLayoutTitleColor);
         }
     }
     private fun initButtons(){
-        findViewById<ImageView>(R.id.btn_orderdetails_back).setOnClickListener{
+        binding.btnOrderdetailsBack.setOnClickListener{
             finish()
         }
 
     }
     private fun initAppBar(){
-        findViewById<AppBarLayout>(R.id.appbar_orderdetails).addOnOffsetChangedListener(OnOffsetChangedListener { appBarLayout, verticalOffset ->
-            val btnready:Button=findViewById<Button>(R.id.btn_orderdetails_orderready)
+        binding.appbarOrderdetails.addOnOffsetChangedListener(OnOffsetChangedListener { appBarLayout, verticalOffset ->
+            val btnready:Button=binding.btnOrderdetailsOrderready
             if (Math.abs(verticalOffset) - appBarLayout.totalScrollRange == 0) {
                 //  Collapsed
                 btnready.visibility= View.INVISIBLE
@@ -55,13 +64,13 @@ class OrderDetailsActivity : AppCompatActivity() {
     }
     private fun initRecyclerView(){
         val adapter = OrderDetailsItemsAdapter(orderDetailsList)
-        val recycler = findViewById<RecyclerView>(R.id.rv_orderdetails)
+        val recycler = binding.rvOrderdetails
         recycler.layoutManager = LinearLayoutManager(this)
         recycler.adapter = adapter
     }
 
     private fun initTotalPrice(){
-        val tvtotal:TextView=findViewById<TextView>(R.id.tv_orderdetails_totalprice)
+        val tvtotal:TextView=binding.tvOrderdetailsTotalprice
         tvtotal.text= tvtotal.text.toString() +"  "+ sumPrice(orderDetailsList).toString()
     }
 
